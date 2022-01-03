f341c0a88d633ebcd02105c2842d1f6ed8de7ffa
andreapesic <75815474+andreapesic@users.noreply.github.com>
2022-01-03 15:05:26 +0100

    Napravljen model AppointmentRating i migracija

---
+++ spa-app/database/migrations/2022_01_03_125644_create_appointments_ratings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppointmentsRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments_ratings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('date_and_time');
+            $table->foreignId('user');
+            $table->foreignId('service');
+            $table->integer('rating');
+            $table->text('note')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments_ratings');
+    }
+}
