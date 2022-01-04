5b650163277f11aa5f7cd2890b5210de0cf874d6
andreapesic <75815474+andreapesic@users.noreply.github.com>
2022-01-04 14:20:58 +0100

    Napravljeni kontroleri

---
+++ spa-app/app/Http/Controllers/AppointmentRatingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AppointmentRating;
+use Illuminate\Http\Request;
+
+class AppointmentRatingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AppointmentRating  $appointmentRating
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AppointmentRating $appointmentRating)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AppointmentRating  $appointmentRating
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AppointmentRating $appointmentRating)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AppointmentRating  $appointmentRating
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AppointmentRating $appointmentRating)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AppointmentRating  $appointmentRating
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AppointmentRating $appointmentRating)
+    {
+        //
+    }
+}
